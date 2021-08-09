a57699b980ba2bacc3ec94648ce869af51a90e40
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-08-09 15:39:52 -0700

    strip unnecessary comments, add clarifications

--- src/lib/components/FormInput.svelte
+++ src/lib/components/FormInput.svelte
@@ -1,30 +1,11 @@
-<!-- Originally uses Formik, but... not the point of this app -->
+<!-- Originally uses Formik, but... not the point of this app
+You'd use svelte-forms-lib instead if you want forms (Formik-like API). Or look for an alternative form lib like Felte,
+or optimally, svelte-use-form. But this app uses very simple validation that has no place using a form lib -->
 <script>
-  // import FormError from '$lib/components/common/FormError'
-  import Input from '$lib/components/common/Input'
+  // import FormError from '$lib/components/common/FormError.svelte'
+  import Input from '$lib/components/common/Input.svelte'
 
   export let ariaLabel, name, type, placeholder, required, value
 </script>
 
 <Input {ariaLabel} {name} {type} {placeholder} {required} bind:value />
-
-<!--
-  const FormInput = ({ ariaLabel, name, type, placeholder }) => {
-    const [field, meta] = useField(name)
-    return (
-      <>
-      <Input
-      field={field}
-      ariaLabel={ariaLabel}
-      name={field.name}
-      type={type}
-      placeholder={placeholder}
-      />
-      {meta.touched && meta.error ? (
-        <FormError text={meta.error}></FormError>
-        ) : null}
-        </>
-        )
-      }
-      
-    -->
