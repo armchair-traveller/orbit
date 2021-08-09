518523ef2ed56d2f25294918351c396b0b2ac88d
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-08-09 15:38:58 -0700

    refactor input and add email type

--- src/lib/components/common/Input.svelte
+++ src/lib/components/common/Input.svelte
@@ -1,27 +1,21 @@
 <script>
   export let ariaLabel, name, type, placeholder, required, value //, field
+  $: params = {
+    'aria-label': ariaLabel,
+    name,
+    placeholder,
+    required,
+    class:
+      'appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5',
+  }
 </script>
 
 <!-- used to have {...field} for formik -->
 
 {#if type == 'text'}
-  <input
-    aria-label={ariaLabel}
-    {name}
-    type="text"
-    class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-    {placeholder}
-    {required}
-    bind:value
-  />
+  <input {...params} type="text" bind:value />
 {:else if type == 'password'}
-  <input
-    aria-label={ariaLabel}
-    {name}
-    type="password"
-    class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-    {placeholder}
-    {required}
-    bind:value
-  />
+  <input {...params} type="password" bind:value />
+{:else if type == 'email'}
+  <input {...params} type="email" bind:value />
 {/if}
