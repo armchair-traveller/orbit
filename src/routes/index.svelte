b01228b7d0e223ce64f622905d2022f6b090042e
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-08-09 17:34:59 -0700

    style

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,7 +15,7 @@
   </div>
 </div>
 <div class="h-full bg-blue-900">
-  <div class="opacity-10">
+  <div class="opacity-30">
     <img
       class="object-fill w-full"
       src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
