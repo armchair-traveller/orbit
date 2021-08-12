40357659a290da4445fcd890fe63aabe886cf43a
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-08-12 00:49:38 -0700

    persist authStore

--- src/lib/stores/AuthStore.js
+++ src/lib/stores/AuthStore.js
@@ -1,19 +1,31 @@
 import { goto } from '$app/navigation'
-import { writable } from 'svelte/store'
+import { get, writable } from 'svelte/store'
 
 function createAuth() {
   if (typeof window == 'undefined')
     return writable({ token: null, expiresAt: null, userInfo: {} })
 
+  const userInfo = localStorage.getItem('userInfo')
   const store = writable({
-    token: null,
-    expiresAt: null,
-    userInfo: {},
+    token: localStorage.getItem('token'),
+    expiresAt: localStorage.getItem('expiresAt'),
+    userInfo: userInfo ? JSON.parse(userInfo) : {},
   })
   const { set } = store
 
   return {
     ...store,
+    setAuthState({ token, userInfo, expiresAt }) {
+      localStorage.setItem('token', token)
+      localStorage.setItem('userInfo', JSON.stringify(userInfo))
+      localStorage.setItem('expiresAt', expiresAt)
+
+      set({ token, userInfo, expiresAt })
+    },
+    isAuthenticated() {
+      get
+      return true
+    },
   }
 }
 
