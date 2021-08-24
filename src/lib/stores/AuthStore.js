6c94182d22fd11a68296286b015e30a734a91f45
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-08-24 01:29:55 -0700

    fix ssr issues

--- src/lib/stores/AuthStore.js
+++ src/lib/stores/AuthStore.js
@@ -2,15 +2,14 @@ import { goto } from '$app/navigation'
 import { get, writable } from 'svelte/store'
 
 function createAuth() {
-  if (typeof window == 'undefined')
-    return writable({ token: null, expiresAt: null, userInfo: {} })
-
-  const userInfo = localStorage.getItem('userInfo')
-  const store = writable({
-    token: localStorage.getItem('token'),
-    expiresAt: localStorage.getItem('expiresAt'),
-    userInfo: userInfo ? JSON.parse(userInfo) : {},
-  })
+  if (typeof window != 'undefined') {
+    var userInfo = localStorage.getItem('userInfo')
+    var store = writable({
+      token: localStorage.getItem('token'),
+      expiresAt: localStorage.getItem('expiresAt'),
+      userInfo: userInfo ? JSON.parse(userInfo) : {},
+    })
+  } else var store = writable({ token: null, expiresAt: null, userInfo: {} })
   const { set } = store
 
   return {
